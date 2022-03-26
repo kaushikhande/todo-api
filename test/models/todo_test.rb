fdef6a4a71eab13f2302bd0df4b8b872acb18679
kaushikhande <kaushikhande302@gmail.com>
2022-03-26 22:24:11 +0530

    added todo table, todo model and todo test

---
+++ test/models/todo_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class TodoTest < ActiveSupport::TestCase
+  test 'validation error for title presence' do
+    todo = Todo.create
+    assert todo.errors
+    assert todo.errors.messages[:title]
+  end
+
+  test 'validation error for schedule presence' do
+    todo = Todo.create(title: 'test')
+    assert todo.errors
+    assert todo.errors.messages[:schedule]
+  end
+
+  test 'creates todo for valid attributes' do
+    assert_difference "Todo.count", 1 do
+      Todo.create title: :test, schedule: Time.current
+    end
+  end
+end
