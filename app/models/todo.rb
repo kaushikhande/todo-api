3e26f0841c310ba40d79fdbc7ec0f24139cd2290
Kaushik Hande <kaushikhande@users.noreply.github.com>
2022-04-02 18:30:19 +0530

    added validation for schedule (#5)
    * added validation for schedule
    
    * description correction

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
+# class Todo: title, description, done, schedule
 class Todo < ApplicationRecord
   validates :title, presence: true
   validates :schedule, presence: true
+  validate :schedule_greater_than_today
+
+  private
+
+  def schedule_greater_than_today
+    return if schedule.blank?
+
+    errors.add(:schedule, 'must be greater than today') if schedule < DateTime.current.beginning_of_day
+  end
 end
