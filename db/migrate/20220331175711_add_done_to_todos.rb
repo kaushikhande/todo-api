6861ad0377a65135341add7a004792a8f60c83b7
kaushikhande <kaushikhande302@gmail.com>
2022-04-02 18:26:11 +0530

    added validation for schedule

--- db/migrate/20220331175711_add_done_to_todos.rb
+++ db/migrate/20220331175711_add_done_to_todos.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Add done boolean column to todos table.
 class AddDoneToTodos < ActiveRecord::Migration[7.0]
   def change
     add_column :todos, :done, :boolean, default: false
--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -5,13 +5,13 @@ require 'test_helper'
 class TodoTest < ActiveSupport::TestCase
   test 'validation error for title presence' do
     todo = Todo.create
-    assert todo.errors
+    assert_not todo.valid?
     assert todo.errors.messages[:title]
   end
 
   test 'validation error for schedule presence' do
     todo = Todo.create(title: 'test')
-    assert todo.errors
+    assert_not todo.valid?
     assert todo.errors.messages[:schedule]
   end
 
@@ -19,5 +19,11 @@ class TodoTest < ActiveSupport::TestCase
     assert_difference 'Todo.count', 1 do
       Todo.create title: :test, schedule: Time.current
     end
+  end
+
+  test 'validation error for schedule greater than or equal today' do
+    todo = Todo.create title: :test, schedule: DateTime.current - 1
+    assert_not todo.valid?
+    assert todo.errors.full_messages.first.match?(/schedule/i)
   end
 end
